666dfb1d3a8a491b5687b66a3e31b0998381c36c
ChrisWoo0443 <chefwoodini@gmail.com>
2024-10-13 00:50:18 -0700

    found values for blue and green marker

--- color.py
+++ color.py
@@ -7,12 +7,14 @@ import numpy as np
 img = cv2.VideoCapture(1)
 frameWidth = 1920
 frameHeight = 1080
-
 img.set(3, frameWidth)
 img.set(4, frameHeight)
-img.set(10,150)
+img.set(10, 150)
+
+
 def empty(a):
     pass
+
 
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV", frameWidth, frameHeight)
@@ -35,11 +37,23 @@ while True:
     s_max = cv2.getTrackbarPos("Saturation max", "HSV")
     v_min = cv2.getTrackbarPos("Value min", "HSV")
     v_max = cv2.getTrackbarPos("Value max", "HSV")
-    # print(h_min, h_max, s_min, s_max, v_min, v_max)
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
 
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    result = cv2.bitwise_and(flipped, flipped, mask=mask)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
+    hStack = np.hstack([flipped, mask, result])
+    cv2.imshow("HSV", hStack)
 
     cv2.imshow('frame', flipped)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+img.release()
+cv2.destroyAllWindows()
+
+#green marker 41, 57, 55, 255, 99, 255
+#blue marker 80, 106, 33, 160, 0 , 255
