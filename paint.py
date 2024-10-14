a6983b50f731a70bc7bf5c09e1584eb40808b737
ChrisWoo0443 <chefwoodini@gmail.com>
2024-10-13 18:16:37 -0700

    added getContours method

--- paint.py
+++ paint.py
@@ -12,9 +12,17 @@ img.set(10, 150)
 myColors = [[41, 57, 55, 255, 99, 255], [80, 106, 33, 160, 0, 255]]
 
 myColorsValue = []
+
 def getContours(img):
-
-
+    contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    x,y,w,h = 0,0,0,0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 500:
+            perimeter = cv2.arcLength(i,True)
+            approximate = cv2.approxPolyDP(i,0.01*perimeter,True)
+            x,y,w,h = cv2.boundingRect(approximate)
+    return x+w//2, y
 
 def getColors(img, myColors, myColorValues):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
